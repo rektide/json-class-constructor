68af69457cd5765f76f3dbfede07b7160a876d20
rektide de la faye <rektide@voodoowarez.com>
2017-03-28 01:01:03 -0400

    Produce the terms for a class.

--- jsonld-class-constructor.js
+++ jsonld-class-constructor.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
+"use strict"
 var
+  iterableMap= require( "iterable-map"),
   jsonld= require( "jsonld").promises,
   prefixes= require( "./prefixes")
 
-var prettyPrint= o=> { console.log(JSON.stringify(o, null, "\t")); return o }
+var prettyPrint= o=> { console.log( JSON.stringify( o, null, "\t")); return o }
 var Type= {
 	Class: prefixes.rdfs + "Class",
 	Property: prefixes.rdfs + "Property",
@@ -32,23 +34,52 @@ class JsonldClassConstructor{
 		this._classProperties= {}
 		this._ontologies= {}
 
-		this._add= node=> {
+		this._add= this._add.bind( this)
+		this._classMap= iterableMap( name=> this._classContext( this._classes[ name]))
+	}
+
+	// Add a new `node` of information to what JsonldClassConstructor knows about
+	// @private
+	_add( node){
+		var
+		  type= node[ "@type"],
+		  label= node[ prefixes.rdfs+ "label"]
+		label= label&& label[ "@value"]|| label
+		if( isOrIncludes( type, Type.Class)){
+			this._classes[ label]= node
+		}else if( isOrIncludes( type, Type.Property)){
 			var
-			  type= node[ "@type"],
-			  label= node[ prefixes.rdfs+ "label"]
-			label= label&& label[ "@value"]|| label
-			if( isOrIncludes( type, Type.Class)){
-				this._classes[ label]= node
-			}else if( isOrIncludes( type, Type.Property)){
-				var
-				  domain= node[ prefixes.rdfs+ "domain"][ "@id"],
-				  classProperties= this._classProperties[ domain]|| (this._classProperties[ domain]= {})
-				classProperties[ label]= node
-			}else if( isOrIncludes( type, Type.Vocab)){
-				this._ontologies[ node[ "@id"]]= node
-			}
+			  domain= node[ prefixes.rdfs+ "domain"][ "@id"],
+			  classProperties= this._classProperties[ domain]|| (this._classProperties[ domain]= {})
+			classProperties[ label]= node
+		}else if( isOrIncludes( type, Type.Vocab)){
+			this._ontologies[ node[ "@id"]]= node
+		
 		}
 	}
+	// Create a JSON-LD context of terms for a `klass` node.
+	// @private
+	_classContext( klass){
+		var
+		  classId= klass[ "@id"],
+		  props= this._classProperties[ classId],
+		  retVal= {}
+		for( var label in props){
+			var
+			  prop= props[ label],
+			  val= prop[ "@id"],
+			  container= prop[ "@container"]
+			if( prop[ "@container"]){
+				val= {
+					"@id": val,
+					"@container": container
+				}
+			}
+			retVal[ label]= val
+		}
+		return retVal
+	}
+
 	load( doc){
 		var expanded= jsonld.compact( doc, {})
 			.then( graph=> graph[ "@graph"].forEach( this._add))
@@ -62,8 +93,12 @@ class JsonldClassConstructor{
 		}
 		return this._ready
 	}
+	[Symbol.iterator](){
+		return this._classMap( this._classes)
+	}
 	getClass( name){
-		return this.ready.then(()=> this._classes[ name])
+		//return this.ready.then(()=> this._classes[ name])
+		return this.ready.then(()=> this._classContext( this._classes[ name]))
 	}
 }
 
@@ -78,20 +113,3 @@ if( require.main=== module){
 	cc.load( doc)
 	cc.getClass( process.argv[ 3]).then( prettyPrint)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
